fb9b8da8cd2bb913db04609ceea63b284063813b
bear <springbear2020@163.com>
2022-03-05 10:15:40 +0800

    Add the function of if user want to access to the background management, he/her must log in out website first.

---
+++ src/main/java/com/bear/book/filter/ManagerFilter.java
@@ -0,0 +1,34 @@
+package com.bear.book.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @author Spring-_-Bear
+ * @datetime 2022/3/5 9:53
+ */
+public class ManagerFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpSession session = req.getSession();
+        Object user = session.getAttribute("user");
+
+        if (user == null) {
+            req.getRequestDispatcher("/pages/user/login.jsp").forward(servletRequest, servletResponse);
+            return;
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
